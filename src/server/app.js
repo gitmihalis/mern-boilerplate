b409743e2d08f2ce877ef0564d0d62b1111a9c34
gitmihalis <mihaliswastaken@protonmail.com>
2018-08-24 19:02:12 -0700

    Init commit

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import path from 'path';
+
 const app = express();
+const publicPath = path.resolve(__dirname, '..', '..', 'public');
+
+app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
    res.send('Hello from Express!');
 });
 
 app.listen(3000, () => {
-   console.log('MERN Boilerplate listening on port 3000!');
+   console.log('👂 MERN Boilerplate listening on port 3000');
+   console.log(`👀 MERN Boilerplate looking inside ${publicPath}`);
 });
